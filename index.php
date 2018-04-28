9472fcee2ec9dc22908ca7a6fad84f352a48b610
Bryan Henson <b.henson915@gmail.com>
2018-04-28 08:42:01 -0500

    updated images for menu

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="description" content=".">
+  <meta name="keywords" content=" BFF, Asian, Restaurants, Restaurant, Chinese Food, Chinese Restaurant, Asian Food, Bar, Sports Bar, Arlington, Texas, Arlington Texas.">
+
 
   <link rel="icon" href="img/BFF-Logo.png">
   <title>BFF Asian Grill and Sports Bar</title>
