b386fdab80978aa40fd1b151bbe39d2bd2cf5bfd
Angie Dinh <minh.dinh@mavs.uta.edu>
2018-04-30 09:02:44 -0500

    changes to franchise.php

--- franchise.php
+++ franchise.php
@@ -17,167 +17,308 @@
 	<?php echo $nav ?>
 
 
-  <div class="hero">
-    <img src="img\BFF-Exterior-Background.jpg" alt="">
-  </div>
-
-  <div class="container" style="margin: 0;">
-    <div class="center">
-      <div class="row">
-        <div class="col blue circle">
-          <img src="img/menu/BFF-Raindow-Roll-1024x647.jpg" alt="" class="circle-img">
-        </div>
-        <div class="col wrapper vertical-center" style="height: 600px; padding: 2em;">
-          <h1>OUR PHILOSOPHY TO SUCCESS</h1>
-          <p>Our philosophy to success revolves around our main priorities.  Our First & Foremost priority is quality.  If you’ve visited our establishment you’ll understand the acronym BFF can stand for so much.  We understand that he trend in the food industry has driven the consumer to seek new experiences.  We offer our patrons the ability to try new things and feel comfortable through quality entrees, the best service & amenities.  BFF stands for the best food for friends & family.  Whether you’d like to sit outdoors and enjoy one of the may draught beers while reading a book or simply take in the nice weather with a cup of joe, ALL are welcome.</p>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div class="franchise"><img src="img/BFF-Exterior-Background.jpg" alt="bff, arlington" style="width:100%; opacity: 0.8;"><div class="franchise-text"><h1 class="text-1">Franchise Opportunities</h1></div></div>
 
 
-  <div class="container" style="margin: 0;">
-    <div class="center">
-      <div class="row">
-        <div class="col wrapper vertical-center" style="height: 600px; padding: 2em;">
-          <h1>HOW MUCH WILL IT COST?</h1>
-          <p>The Initial Franchise Fee is $35,000 for the first restaurant. There is a franchise fee discount for additional restaurants if you enter into a multi-unit Development Agreement. The estimated total cost of opening your very own BFF is $613,000 to $1,137,500. Please take a look at our chart for more details. If you have any questions, send us a message or fill out our questionnaire to begin the process. For more information, check out our F.A.Q.</p>
-        </div>
-        <div class="col blue circle">
-          <img src="img\hero3.jpg" alt="" class="circle-img">
-        </div>
-      </div>
-    </div>
-  </div>
+	<div onclick="myFunction()" class="toggle-click">OUR PHILOSOPHY TO SUCCESS</div>
 
-  <table id="feeTable">
-    <thead>
-      <tr>
-        <td><strong>Type of Expenditure</strong></td>
-        <td><strong>Estimated Amount</strong></td>
-        <td><strong>Method of Payment</strong></td>
-        <td><strong>When Due</strong></td>
-        <td><strong>To Whom Payment is Due</strong></td>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Initial Franchise Fee</td>
-        <td>$35,000</td>
-        <td>Lump sum</td>
-        <td>At Contract Signing</td>
-        <td>Us</td>
-      </tr>
-      <tr>
-        <td>Rent, Utility, Security Deposits &amp; Business Licenses</td>
-        <td>$20,000 to $40,000</td>
-        <td>As arranged</td>
-        <td>As incurred</td>
-        <td>Landlord, Utilities, etc.</td>
-      </tr>
-      <tr>
-        <td>Architect &amp; Design Fees</td>
-        <td>$18,000 to $24,000</td>
-        <td>As arranged</td>
-        <td>As incurred</td>
-        <td>Contractors &amp; Suppliers</td>
-      </tr>
-      <tr>
-        <td>Leasehold Improvements</td>
-        <td>$275,000 to $500,000</td>
-        <td>As arranged</td>
-        <td>As incurred</td>
-        <td>Contractors and third party suppliers</td>
-      </tr>
-      <tr>
-        <td>Signage</td>
-        <td>$15,000 to $35,000</td>
-        <td>As arranged</td>
-        <td>As incurred</td>
-        <td>Approved suppliers</td>
-      </tr>
-      <tr>
-        <td>Furniture &amp; Fixtures</td>
-        <td>$35,000 to $55,000</td>
-        <td>As arranged</td>
-        <td>As incurred</td>
-        <td>Approved suppliers</td>
-      </tr>
-      <tr>
-        <td>Equipment &amp; Small wares</td>
-        <td>$100,000 to $190,000</td>
-        <td>As arranged</td>
-        <td>As incurred</td>
-        <td>Approved suppliers</td>
-      </tr>
-      <tr>
-        <td>Point of Sale &amp; Security Systems</td>
-        <td>$24,000 to $33,000</td>
-        <td>As arranged</td>
-        <td>As incurred</td>
-        <td>Approved suppliers</td>
-      </tr>
-        <tr>
-        <td>Office Equipment &amp; Supplies</td>
-        <td>$2,500 to $5,500</td>
-        <td>As arranged</td>
-        <td>As incurred</td>
-        <td>Approved suppliers</td>
-      </tr>
-      <tr>
-        <td>Professional fees</td>
-        <td>$12,000 to $19,000</td>
-        <td>As arranged</td>
-        <td>As incurred</td>
-        <td>Accountant, Attorney &amp; Others</td>
-      </tr>
-      <tr>
-        <td>Initial Inventory</td>
-        <td>$8,000 to $15,000</td>
-        <td>As arranged</td>
-        <td>As incurred</td>
-        <td>Approved suppliers</td>
-      </tr>
-      <tr>
-        <td>Insurance</td>
-        <td>$12,000 to $20,000</td>
-        <td>As agent requires</td>
-        <td>Before opening</td>
-        <td>Insurance Carriers</td>
-      </tr>
-      <tr>
-        <td>Pre-opening training expenses</td>
-        <td>$19,000to $36,000</td>
-        <td>As arranged</td>
-        <td>Before opening</td>
-        <td>BFF &amp; Staff</td>
-      </tr>
-      <tr>
-        <td>Grand Opening Advertising</td>
-        <td>$8,000 to $15,000</td>
-        <td>Lump sum</td>
-        <td>Before opening</td>
-        <td>Vendors, Service &amp; Media Providers</td>
-      </tr>
-      <tr>
-        <td>Additional Funds (6 months)</td>
-        <td>$30,000 to $75,000</td>
-        <td>As arranged</td>
-        <td>As incurred</td>
-        <td>Your Own Business Bank Account</td>
-      </tr>
-      </tbody>
-      <tfoot>
-      <tr>
-      <td id="imageCell"><strong>Approximate Total Cost</strong></td>
-      <td style="text-align: right;" colspan="4"><strong>$613,000 to $1,137,500</strong></td>
-      </tr>
-    </tfoot>
-  </table>
+	<div id="myDIV">
+	Our philosophy to success revolves around our main priorities.  Our First &amp; Foremost priority is quality.  If you’ve visited our establishment you’ll understand the acronym BFF can stand for so much.  We understand that he trend in the food industry has driven the consumer to seek new experiences.  We offer our patrons the ability to try new things and feel comfortable through quality entrees, the best service &amp; amenities.  BFF stands for the best food for friends & family.  Whether you’d like to sit outdoors and enjoy one of the may draught beers while reading a book or simply take in the nice weather with a cup of joe, ALL are welcome.
+	</div>
+
+	<div onclick="myFunc()" class="toggle-click">HOW MUCH WILL IT COST?</div>
+
+	<div id="myDIV2">
+	The Initial Franchise Fee is $35,000 for the first restaurant. There is a franchise fee discount for additional restaurants if you enter into a multi-unit Development Agreement. The estimated total cost of opening your very own BFF is $613,000 to $1,137,500. Please take a look at our chart for more details. If you have any questions, send us a message or fill out our questionnaire to begin the process. For more information, check out our F.A.Q.
+	</div>
 
 
-  <?php echo $footer ?>
+	<table class="franchise-table">
+		<caption>Startup costs &amp; fees</caption>
+		<tr>
+			<th>Type of Expenditure</th>
+			<th>Estimated Amount</th>
+			<th>Method of Payment</th>
+			<th>Due</th>
+			<th>Payment Made To</th>
+		</tr>
+		<tr>
+			<td>Initial Franchise Fee</td>
+			<td>$35,000</td>
+			<td>Lump sum</td>
+			<td>At Contract Signing</td>
+			<td>BFF</td>
+		</tr>
+		<tr>
+			<td>Rent, Utility, Security Deposits &amp; Business Licenses</td>
+			<td>$20,000 to $40,000</td>
+			<td>As arranged</td>
+			<td>As incurred</td>
+			<td>Landlord, Utilities, etc.</td>
+		</tr>
+		<tr>
+			<td>Architect &amp; Design Fees</td>
+			<td>$18,000 to $24,000</td>
+			<td>As arranged</td>
+			<td>As incurred</td>
+			<td>Contractors &amp; Suppliers</td>
+		</tr>
+		<tr>
+			<td>Leasehold Improvements</td>
+			<td>$275,000 to $500,000</td>
+			<td>As arranged</td>
+			<td>As incurred</td>
+			<td>Contractors and third party suppliers</td>
+		</tr>
+		<tr>
+			<td>Signage</td>
+			<td>$15,000 to $35,000</td>
+			<td>As arranged</td>
+			<td>As incurred</td>
+			<td>Approved suppliers</td>
+		</tr>
+		<tr>
+			<td>Furniture &amp; Fixtures</td>
+			<td>$35,000 to $55,000</td>
+			<td>As arranged</td>
+			<td>As incurred</td>
+			<td>Landlord, Utilities, etc.</td>
+		</tr>
+		<tr>
+			<td>Equipment &amp; Small wares</td>
+			<td>$100,000 to $190,000</td>
+			<td>As arranged</td>
+			<td>As incurred</td>
+			<td>Landlord, Utilities, etc.</td>
+		</tr>
+		<tr>
+			<td>Point of Sale &amp; Security Systems</td>
+			<td>$24,000 to $33,000</td>
+			<td>As arranged</td>
+			<td>As incurred</td>
+			<td>Landlord, Utilities, etc.</td>
+		</tr>
+		<tr>
+			<td>Office Equipment &amp; Supplies</td>
+			<td>$2,500 to $5,500</td>
+			<td>As arranged</td>
+			<td>As incurred</td>
+			<td>Landlord, Utilities, etc.</td>
+		</tr>
+		<tr>
+			<td>Professional fees</td>
+			<td>$12,000 to $19,000</td>
+			<td>As arranged</td>
+			<td>As incurred</td>
+			<td>Accountant, Attorney &amp; Others</td>
+		</tr>
+		<tr>
+			<td>Initial Inventory</td>
+			<td>$8,000 to $15,000</td>
+			<td>As arranged</td>
+			<td>As incurred</td>
+			<td>Approved suppliers</td>
+		</tr>
+		<tr>
+			<td>Insurance</td>
+			<td>$12,000 to $20,000</td>
+			<td>As agent requires</td>
+			<td>Before opening</td>
+			<td>Insurance Carriers</td>
+		</tr>
+		<tr>
+			<td>Office Equipment &amp; Supplies</td>
+			<td>$2,500 to $5,500</td>
+			<td>As arranged</td>
+			<td>As incurred</td>
+			<td>Landlord, Utilities, etc.</td>
+		</tr>
+		<tr>
+			<td>Office Equipment &amp; Supplies</td>
+			<td>$2,500 to $5,500</td>
+			<td>As arranged</td>
+			<td>As incurred</td>
+			<td>Landlord, Utilities, etc.</td>
+		</tr>
+		<tr>
+			<td>Pre-opening training expenses</td>
+			<td>$19,000 to $36,000	</td>
+			<td>As arranged</td>
+			<td>Before opening</td>
+			<td>BFF &amp; Staff</td>
+		</tr>
+		<tr>
+			<td>Grand Opening Advertising</td>
+			<td>$8,000 to $15,000</td>
+			<td>Lump sum</td>
+			<td>Before opening</td>
+			<td>Vendors, Service &amp; Media Providers</td>
+		</tr>
+		<tr>
+			<td>Additional Funds (6 months)</td>
+			<td>$30,000 to $75,000</td>
+			<td>As arranged</td>
+			<td>As incurred</td>
+			<td>Your Own Business Bank Account</td>
+		</tr>
+		<tr>
+			<td>Approximate Total Cost</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td>$613,000 to $1,137,500</td>
+		</tr>
+	</table>
 
-  <script src="js/main.js"></script>
-</body>
-</html>
+
+<button id="form-btn">FILL OUT FORM</button>
+
+	<div id="myModal" class="modal">
+
+			<!-- Modal content -->
+			<div class="modal-content">
+				<span class="close">&times;</span>
+
+	<div class="franchise-form">
+		<form action="/action_page.php">
+				<input type="text" name="firstname" value="First Name *">
+				<input type="text" name="lastname" value="Last Name *"><br>
+				<input type="text" name="address" value="Address *"><br>
+				<input type="text" name="streetAddress" value="Street Address"><br>
+				<input type="text" name="city" value="City"><br>
+				State:
+					<select id="state" name="state">
+						<option value="Alabama">Alabama</option>
+						<option value="Alaska">Alaska</option>
+						<option value="Arizona">Arizona</option>
+						<option value="Arkansas">Arkansas</option>
+						<option value="California">California</option>
+						<option value="Colorado">Colorado</option>
+						<option value="Connecticut">Connecticut</option>
+						<option value="Delaware">Delaware</option>
+						<option value="Florida">Florida</option>
+						<option value="Georgia">Georgia</option>
+						<option value="Hawaii">Hawaii</option>
+						<option value="Idaho">Idaho</option>
+						<option value="Illinois">Illinois</option>
+						<option value="Indiana">Indiana</option>
+						<option value="Iowa">Iowa</option>
+						<option value="Kansas">Kansas</option>
+						<option value="Kentucky">Kentucky</option>
+						<option value="Louisiana">Louisiana</option>
+						<option value="Maine">Maine</option>
+						<option value="Maryland">Maryland</option>
+						<option value="Massachusetts">Massachusetts</option>
+						<option value="Michigan">Michigan</option>
+						<option value="Minnesota">Minnesota</option>
+						<option value="Mississippi">Mississippi</option>
+						<option value="Missouri">Missouri</option>
+						<option value="Montana">Montana</option>
+						<option value="Nebraska">Nebraska</option>
+						<option value="Nevada">Nevada</option>
+						<option value="NewHampshire">New Hampshire</option>
+						<option value="NewJersey">New Jersey</option>
+						<option value="NewMexico">New Mexico</option>
+						<option value="NewYork">New York</option>
+						<option value="NorthCarolina">North Carolina</option>
+						<option value="NorthDakota">North Dakota</option>
+						<option value="Ohio">Ohio</option>
+						<option value="Oklahoma">Oklahoma</option>
+						<option value="Oregon">Oregon</option>
+						<option value="Pennsylvania">Pennsylvania</option>
+						<option value="RhodeIsland">Rhode Island</option>
+						<option value="SouthCarolina">South Carolina</option>
+						<option value="SouthDakota">South Dakota</option>
+						<option value="Tennessee">Tennessee</option>
+						<option value="Texas">Texas</option>
+						<option value="Utah">Utah</option>
+						<option value="Vermont">Vermont</option>
+						<option value="Virginia">Virginia</option>
+						<option value="Washington">Washington</option>
+						<option value="WestVirginia">West Virginia</option>
+						<option value="Wisconsin">Wisconsin</option>
+						<option value="Wyoming">Wyoming</option>
+			</select><br>
+			<input type="text" name="zipcode" value="Zip Code"><br>
+			<input type="text" name="Phone" value="Phone *"><br>
+			<input type="text" name="Email" value="Email *"><br>
+			Are you interested in a single store or multiple locations? * <br>
+			<input type="radio" name="location">Single<br>
+			<input type="radio" name="location">Multiple<br>
+			Possible Location or Locations? *<br>
+			<textarea row="4" cols="50">
+			</textarea><br>
+			Projected Date of Opening? *<br>
+			<input type="text" name="date" value="--/--/----"><br>
+			What is your approximate net worth? *<br>
+			<input type="text" name="networth" value=""><br>
+			Who would operate your franchise on a day-to-day basis? *<br>
+			<input type="text" name="operate" value=""><br>
+			What are you looking for in a franchise opportunity?<br>
+			<textarea row="4" cols="50">
+			</textarea><br>
+			Tell us about your past or current relevant business experience.<br>
+			<textarea row="4" cols="50">
+			</textarea><br>
+
+			<input type="submit" value="Submit">
+		</form>
+
+	</div>
+
+</div>
+
+
+	</div>
+
+
+
+
+	<?php echo $footer ?>
+
+<script>
+
+function myFunction() {
+		var x = document.getElementById("myDIV");
+		if (x.style.display === "none") {
+				x.style.display = "block";
+		} else {
+				x.style.display = "none";
+		}
+}
+
+function myFunc() {
+		var x = document.getElementById("myDIV2");
+		if (x.style.display === "none") {
+				x.style.display = "block";
+		} else {
+				x.style.display = "none";
+		}
+}
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("form-btn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+	modal.style.display = "block";
+}
+
+span.onclick = function() {
+	modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+	if (event.target == modal) {
+			modal.style.display = "none";
+	}
+}
+
+
+
+</script>
--- locations.php
+++ locations.php
@@ -16,90 +16,66 @@
 <body>
 	<?php echo $nav ?>
 
-  <div class="container">
-    <div class="wrapper">
-      <div class="row">
-        <div class="col-6 vertical-center">
-          <h1>Arlington Location</h1>
-          2150 E Lamar Blvd #100, Arlington, TX 76006
+	<table>
+	  <tr>
+	    <th><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3355.152196076336!2d-97.07648428520461!3d32.76169589207451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e876b81187443%3A0x8e2cd83655d422a7!2sBFF+Asian+Grill+and+Bar!5e0!3m2!1sen!2sus!4v1525074433338" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</th>
+	    <th><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d107456.7002693665!2d-97.16279017737968!3d32.68546362156901!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e618aa14e7f0f%3A0x16155eda5f3ef21d!2sBFF+Grill+and+Sports+Bar!5e0!3m2!1sen!2sus!4v1525075361277" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></th>
+	  </tr>
+	  <tr>
+	    <td>
+				<p>
+					<h1 class="location">ARLINGTON</h1>
 
-          <a href="tel: 817-385-0760">(817) 385-0760</a>
+						2150 E. Lamar Blvd. Suite 100<br/>
+						Arlington, Texas 76006
+				</p>
+			</td>
+	    <td>
+				<p>
+					<h1 class="location">MANSFIELD</h1>
 
-          <div class="hours">
-            <h3>Hours</h3>
-            <b>Sunday</b><br> 
-            11AM–12AM<br>
+						1601 Debbie Ln<br/>
+						Mansfield, Texas 76063
+				</p>
 
-            <b>Monday</b><br> 
-            11AM–12AM<br>
-
-            <b>Tuesday</b><br> 
-            11AM–12AM<br>
-
-            <b>Wednesday</b><br> 
-            11AM–12AM<br>
-
-            <b>Thursday</b><br> 
-            11AM–12AM<br>
-
-            <b>Friday</b><br> 
-            11AM–2AM<br>
-
-            <b>Saturday</b><br> 
-            11AM–2AM<br>
-          </div>
-          
-        </div>
-        <div class="col-6 vertical-center">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3355.1521960763935!2d-97.07648428451958!3d32.76169589207299!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e876b81187443%3A0x8e2cd83655d422a7!2sBFF+Asian+Grill+and+Bar!5e0!3m2!1sen!2sus!4v1525028480248" width="600" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <hr>
-
-  <div class="container">
-    <div class="wrapper">
-      <div class="row">
-        <div class="col-6 vertical-center">
-          <h1>Mansfield Location</h1>
-          1601 Debbie Ln, Mansfield, TX 76063
-
-          <a href="tel: 817-473-7233">(817) 473-7233</a>
-
-          <div class="hours">
-            <h3>Hours</h3>
-            <b>Sunday</b><br> 
-            11AM–10:30PM<br>
-
-            <b>Monday</b><br> 
-            11AM–10:30PM<br>
-
-            <b>Tuesday</b><br> 
-            11AM–10:30PM<br>
-
-            <b>Wednesday</b><br> 
-            11AM–10:30PM<br>
-
-            <b>Thursday</b><br> 
-            11AM–10:30PM<br>
-
-            <b>Friday</b><br> 
-            11AM–11PM<br>
-
-            <b>Saturday</b><br> 
-            11AM–11PM<br>
-
-          </div>
-          
-        </div>
-        <div class="col-6 vertical-center">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d107456.70038525366!2d-97.16296115595347!3d32.685463525266066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e618aa14e7f0f%3A0x16155eda5f3ef21d!2sBFF+Grill+and+Sports+Bar!5e0!3m2!1sen!2sus!4v1525029521808" width="600" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
-        </div>
-      </div>
-    </div>
-  </div>
+			</td>
+	  </tr>
+	  <tr>
+	    <td><img src="img/icons/phone.png" height="20" width="20"> (817)385-0760</td>
+	    <td><img src="img/icons/phone.png" height="20" width="20"> (817)473-7233</td>
+	  </tr>
+	  <tr>
+	  	<td>
+				<div class="business-hours">
+				<h2 class="title">Opening Hours</h2>
+				<ul class="list-unstyled opening-hours">
+				<li>Sunday <span class="pull-right">11:00-12:00AM</span></li>
+				<li>Monday <span class="pull-right">11:00-12:00AM</span></li>
+				<li>Tuesday <span class="pull-right">11:00-12:00AM</span></li>
+				<li>Wednesday <span class="pull-right">11:00-12:00AM</span></li>
+				<li>Thursday <span class="pull-right">11:00-12:00AM</span></li>
+				<li>Friday <span class="pull-right">11:00-2:00AM</span></li>
+				<li>Saturday <span class="pull-right">11:00-2:00AM</span></li>
+				</ul>
+				</div>
+			</td>
+	    <td>
+					<div class="business-hours">
+					<h2 class="title">Opening Hours</h2>
+					<ul class="list-unstyled opening-hours">
+					<li>Sunday <span class="pull-right">11:00-10:30PM</span></li>
+					<li>Monday <span class="pull-right">11:00-10:30PM</span></li>
+					<li>Tuesday <span class="pull-right">11:00-10:30PM</span></li>
+					<li>Wednesday <span class="pull-right">11:00-10:30PM</span></li>
+					<li>Thursday <span class="pull-right">11:00-10:30PM</span></li>
+					<li>Friday <span class="pull-right">11:00-11:00PM</span></li>
+					<li>Saturday <span class="pull-right">11:00-11:00PM</span></li>
+					</ul>
+					</div>
+				</td>
+	  </tr>
+	</table>
 
   <?php echo $footer ?>
 
